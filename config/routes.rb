f39820ace59ce9d5cbc60ffe15abc6e6c99b56ed
Zach Grande <zach.grande23@gmail.com>
2024-02-20 14:49:12 -0800

    Create company and user

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   Rails.application.routes.draw do
+    devise_for :users
     resources :quotes
   end
 end
